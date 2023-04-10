f1fc6dc1117b1b792820e45a7fdc5c77934dd457
valentinab25 <valentinab25>
2023-04-10 18:08:14 +0300

    Fix permissions

--- run_geoupdate.sh
+++ run_geoupdate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ -d /geoupdate ] && [ -f /geoupdate/GeoLite2-City.mmdb ]; then
+if [ -d "/geoupdate" ] && [ -f "/geoupdate/GeoLite2-City.mmdb" ]; then
 
 	cp /geoupdate/GeoLite2-City.mmdb /opt/bitnami/matomo/misc/
+	chown daemon:root /opt/bitnami/matomo/misc/GeoLite2-City.mmdb
 fi
 
