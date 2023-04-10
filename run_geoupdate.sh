52d02c7556eb52f23e25fa75309779cf813a5408
valentinab25 <valentinab25>
2023-04-10 18:15:47 +0300

    fix spacing

--- run_geoupdate.sh
+++ run_geoupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -d "/geoupdate" ] && [ -f "/geoupdate/GeoLite2-City.mmdb" ]; then
-  cp /geoupdate/GeoLite2-City.mmdb /opt/bitnami/matomo/misc/
+if [ -d "/geoupdate" ] && [ -f "/geoupdate/GeoLite2-City.mmdb" ]; then
+  cp /geoupdate/GeoLite2-City.mmdb /opt/bitnami/matomo/misc/
   chown daemon:root /opt/bitnami/matomo/misc/GeoLite2-City.mmdb
 fi
 
