ce6081b729736413819b069111efd8039d3efc1e
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2023-04-10 18:17:34 +0300

    Update run_geoupdate.sh

--- run_geoupdate.sh
+++ run_geoupdate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ -d "/geoupdate" ] && [ -f "/geoupdate/GeoLite2-City.mmdb" ]; then
-  cp /geoupdate/GeoLite2-City.mmdb /opt/bitnami/matomo/misc/
-  chown daemon:root /opt/bitnami/matomo/misc/GeoLite2-City.mmdb
+ cp /geoupdate/GeoLite2-City.mmdb /opt/bitnami/matomo/misc/
+ chown daemon:root /opt/bitnami/matomo/misc/GeoLite2-City.mmdb
 fi
-
