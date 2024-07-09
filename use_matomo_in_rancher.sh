eb2871c7dbc8ab1c71491b3d439b438e973d0d72
valentinab25 <valentinab25>
2024-07-09 16:58:13 +0300

    Fix patch path

--- use_matomo_in_rancher.sh
+++ use_matomo_in_rancher.sh
@@ -21,16 +21,14 @@ sed -i "s/LogFormat \"%h/LogFormat \"%{X-Forwarded-For}i/g" /opt/bitnami/apache/
 # Apply patch from https://github.com/plone/volto/pull/5607.
 # Prevent setting pk_ref cookie with an empty string; it is perceived as SQL injection attempt.
 
-rm -rf "/opt/bitnami/matomo/matomo.js"
-cp /opt/bitnami/matomo/github-pr-22071/matomo.js /opt/bitnami/matomo/
-rm -rf "/opt/bitnami/matomo/piwik.js"
-cp /opt/bitnami/matomo/github-pr-22071/piwik.js /opt/bitnami/matomo/
-rm -rf "/opt/bitnami/matomo/js/piwik.js"
-cp /opt/bitnami/matomo/github-pr-22071/js/piwik.js /opt/bitnami/matomo/js/
-rm -rf "/opt/bitnami/matomo/js/piwik.min.js"
-cp /opt/bitnami/matomo/github-pr-22071/js/piwik.min.js /opt/bitnami/matomo/js/
-
-rm -rf /opt/bitnami/matomo/github-pr-22071
+cp /tmp/github-pr-22071/matomo.js /opt/bitnami/matomo/
+cp /tmp/github-pr-22071/matomo.js /bitnami/matomo/
+cp /tmp/github-pr-22071/piwik.js /opt/bitnami/matomo/
+cp /tmp/github-pr-22071/piwik.js /bitnami/matomo/
+cp /tmp/github-pr-22071/js/piwik.js /opt/bitnami/matomo/js/
+cp /tmp/github-pr-22071/js/piwik.js /bitnami/matomo/js/
+cp /tmp/github-pr-22071/js/piwik.min.js /opt/bitnami/matomo/js/
+cp /tmp/github-pr-22071/js/piwik.min.js /bitnami/matomo/js/
 
 # fix file permissions
 
