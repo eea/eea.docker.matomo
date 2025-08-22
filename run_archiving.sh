2ab8202beab3e62d961908fecc278ddc9583b62b
miahi <me@miahi.ro>
2025-08-22 11:34:58 +0300

    Rollback workaround #285375

--- run_archiving.sh
+++ run_archiving.sh
@@ -6,7 +6,3 @@ MATOMO_URL=${MATOMO_URL:-matomo}
 
 
 php /opt/bitnami/matomo/console core:archive --url=http://$MATOMO_URL --concurrent-archivers=$CONCURRENT_ARCHIVERS --concurrent-requests-per-website=$CONCURRENT_REQS_PER_WEBSITE -vvv
-chown -R daemon:daemon /bitnami/matomo/tmp/templates_c/
-su -s /bin/bash daemon -c "php -r 'file_get_contents(\"http://localhost/\");'"
-chown -R daemon:daemon /bitnami/matomo/tmp/templates_c/
-
